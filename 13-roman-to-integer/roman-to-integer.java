c8817e56d50732f9f883de089e36bfddd5e7aa5e
Amit Kumar <105246934+AKamit18@users.noreply.github.com>
2025-02-20 20:06:36 +0530

    Time: 5 ms (56.85%) | Memory: 44.8 MB (68.64%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int romanToInt(String s) {
+
+        // Create a map to store the values of Roman numerals
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        // Iterate through the string from right to left
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char currentChar = s.charAt(i);
+            int currentValue = romanMap.get(currentChar);
+
+            // If the current value is less than the previous value, subtract it
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        return result;
+        
+    }
+}
