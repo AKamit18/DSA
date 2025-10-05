ad2d6ffdc8343c4618f406fef77d0cb820791c2b
Amit Kumar <105246934+AKamit18@users.noreply.github.com>
2025-10-05 05:41:25 +0530

    Time: 4 ms (76.17%) | Memory: 43.3 MB (71.53%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,36 +5,36 @@ class Solution {
             return false;
         }
 
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
+        // char[] sArray = s.toCharArray();
+        // char[] tArray = t.toCharArray();
 
-        Arrays.sort(sArray);
-        Arrays.sort(tArray);
+        // Arrays.sort(sArray);
+        // Arrays.sort(tArray);
 
-        // return Arrays.equals(sArray,tArray);
+        // // return Arrays.equals(sArray,tArray);
 
-        String sortedS = new String(sArray);
-        String sortedT = new String(tArray);
+        // String sortedS = new String(sArray);
+        // String sortedT = new String(tArray);
 
-        return sortedS.equals(sortedT);
+        // return sortedS.equals(sortedT);
 
 
 
-        // int[] arr = new int[26];
+        int[] arr = new int[26];
 
-        // for( int i =0; i<s.length();i++){
-        //     arr[s.charAt(i) - 'a']++;
-        // }
+        for( int i =0; i<s.length();i++){
+            arr[s.charAt(i) - 'a']++;
+        }
 
-        // for( int i=0;i<t.length();i++) {
-        //     arr[t.charAt(i) - 'a']--;
+        for( int i=0;i<t.length();i++) {
+            arr[t.charAt(i) - 'a']--;
 
-        //     if(arr[t.charAt(i) - 'a'] < 0) {
-        //         return false;
-        //     }
-        // }
+            if(arr[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
 
-        // return true;
+        return true;
 
         // boolean flag = true;
         // for(int i=0;i<arr.length; i++) {
