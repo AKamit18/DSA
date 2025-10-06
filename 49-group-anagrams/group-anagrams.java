825492d49cdd57e3afb507b43f165c3500ca44df
Amit Kumar <105246934+AKamit18@users.noreply.github.com>
2025-10-07 01:38:52 +0530

    Time: 17 ms (26.88%) | Memory: 48.8 MB (13.81%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -3,17 +3,42 @@ class Solution {
 
         Map<String, List<String>> map = new HashMap<>();
 
-        for( String str : strs) {
 
-            char[] chars = str.toCharArray();
-            Arrays.sort(chars);
-            String key = new String(chars);
+        for(String str : strs) {
+            int[] count = new int[26];
+            for( char c: str.toCharArray()){
+                count[c-'a']++;
+            }
 
+            StringBuilder key = new StringBuilder();
+            for(int num: count) {
+                key.append(num).append('#');
+            }
 
-             map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+            map.computeIfAbsent(key.toString(), k-> new ArrayList<>()).add(str);
         }
 
         return new ArrayList<>(map.values());
+
+
+        // for( String str : strs) 
+        // {
+        //     char[] chars = str.toCharArray();
+        //     Arrays.sort(chars);
+        //     String key = new String(chars);
+        //     map.computeIfAbsent(key, k -> new ArrayList<>()).add(str); 
+        //     //List<String> list = map.computeIfAbsent(key, k -> new ArrayList<>());
+        //     //list.add(str);
+
+        //     // if (!map.containsKey(key)) {
+        //     //     map.put(key, new ArrayList<>());   // create new list if key not present
+        //     // }
+        //     // map.get(key).add(str);  // add current string to that list
+
+
+        // }
+
+        // return new ArrayList<>(map.values());
         
     }
 }
